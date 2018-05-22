99f1232b044074f5b859d41f0c7c8e7b79b9e754
Kate <ek.tcareva@gmail.com>
2018-05-22 16:14:44 -0700

    ValueError tests added

--- tests/test_counter.py
+++ tests/test_counter.py
@@ -1,5 +1,6 @@
 from event_counter.counter import Counter
 import time
+import pytest
 
 my_counter = Counter()
 
@@ -12,11 +13,13 @@ def test_counter1():
     result = my_counter.count_events()
     assert result==300
 
+
+time.sleep(1)
+time_1 = time.strftime("%d.%m.%Y %H:%M:%S", time.localtime())
+
+'''
 # test 2: count events in an time interval
 def test_counter2():
-
-    time.sleep(1)
-    time_1 = time.strftime("%d.%m.%Y %H:%M:%S", time.localtime())
     time.sleep(1)
     for i in range(5):
         my_counter.add_event()
@@ -24,7 +27,11 @@ def test_counter2():
     time_2 = time.strftime("%d.%m.%Y %H:%M:%S", time.localtime())
     result = my_counter.count_events(time_1, time_2)
     assert result==5
+'''
 
+time.sleep(2)
+time_3 = time.strftime("%d.%m.%Y %H:%M:%S", time.localtime())
+'''
 # test 3: add max number of events
 def test_counter3():
     t0=time.time()
@@ -33,7 +40,7 @@ def test_counter3():
             my_counter.add_event()
     result=time.time()-t0
     print("Adding 300 000 000 events took ",time.time()-t0)
-
+'''
 # test 4: count all events for last 5 minutes (should take no more than 1 sec)
 def test_counter4():
     time.sleep(1)
@@ -43,4 +50,18 @@ def test_counter4():
     print("Counting of ", result, "events took", t1)
     assert t1<1
 
+# test 5: start and end of interval are switched
+def test_counter5():
+    with pytest.raises(ValueError):
+        my_counter.count_events(time_3, time_1)
+
+# test 6: wrong formatting of time
+def test_counter6():
+    with pytest.raises(ValueError):
+        my_counter.count_events("11:05:02", time_1)
+
+# test 7: time is out of supported interval
+def test_counter7():
+    with pytest.raises(ValueError):
+        my_counter.count_events("29.08.2017 11:00:02", "29.08.2017 11:05:02")
 
